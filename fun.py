0496ddcdac391daefa905399f275cf8cc25eb8fc
39xdgy <jwang20@cornellcollege.edu>
2019-12-19 18:55:03 -0600

    Change the background, Add another picture

--- fun.py
+++ fun.py
@@ -11,8 +11,9 @@ Red = (255, 0, 0)
 size = (700, 500)
 window = pygame.display.set_mode(size)
 pygame.display.set_caption("My first Game")
-
-
+#BackG is DP, Background.jpeg is anime background
+image = pygame.image.load("BackG.jpg")
+window.blit(image, (0, 0))
 
 carryOn = True
 
@@ -24,8 +25,8 @@ while carryOn:
             carryOn = False
 
 
-
-    window.fill(White)
+    #uncommon this line to make the background white
+    #window.fill(White)
     pygame.draw.rect(window, Red, [55, 200, 100, 70], 0)
     pygame.draw.line(window, Green, [0, 0], [100, 100], 5)
     pygame.draw.ellipse(window, Black, [20, 20, 250, 100], 2)
