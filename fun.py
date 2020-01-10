1e7b51d9eb6f545b387e9e8801a2bc7fd8b5f674
39xdgy <jwang20@cornellcollege.edu>
2020-01-09 21:01:11 -0600

    add score system, add star, easiest game system created(icon eat star)

--- fun.py
+++ fun.py
@@ -1,76 +1,80 @@
 import pygame
+from random import randint
+
+
+def move(press_flag, list_UDLF, move_range, x, y):
+    if press_flag[list_UDLF[0]]: y -= move_range
+    if press_flag[list_UDLF[1]]: y += move_range
+    if press_flag[list_UDLF[2]]: x -= move_range
+    if press_flag[list_UDLF[3]]: x += move_range
+
+    if x > 1280: x = 0
+    if y > 720: y = 0
+    if x < 0: x = 1280
+    if y < 0: y = 720
+    
+    return x, y
+
+
+def change_score(now_score, num, font):
+    str_score = 'Your Score: ' + str(now_score + num)
+    return font.render(str_score, False, (255, 255, 255)), now_score + num
+
 
 
 pygame.init()
-
-Black = (0, 0, 0)
-White = (255, 255, 255)
-Green = (0, 255, 0)
-Red = (255, 0, 0)
-
 size = (1280, 720)
 window = pygame.display.set_mode(size)
-window.fill(White)
 pygame.display.set_caption("My first Game")
-#BackG is DP, Background.jpeg is anime background
-image = pygame.image.load("Background.jpeg")
-
+Back_image = pygame.image.load("Background.jpeg")
 
 carryOn = True
-
 clock = pygame.time.Clock()
+
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
+score = 0
+str_score = 'Your Score: ' + str(score)
+textsurface = myfont.render(str_score, False, (255, 255, 255))
+
+
+star_x = randint(20, 1260)
+star_y = randint(20, 700)
+
+
+
+
 
 x = 600
 y = 630
-
-fake_x = 120
-fake_y = 120
 
 while carryOn:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             carryOn = False
 
-
-    #uncommon this line to make the background white
-    #window.fill(White)
-    
-    #pygame.draw.line(window, Green, [0, 0], [100, 100], 5)
-    #pygame.draw.ellipse(window, Black, [20, 20, 250, 100], 2)
-
     pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_UP]: y -= 3
-    if pressed[pygame.K_DOWN]: y += 3
-    if pressed[pygame.K_LEFT]: x -= 3
-    if pressed[pygame.K_RIGHT]: x += 3
 
-    #pygame.draw.rect(window, Red, [x, y, 55, 55], 0)
-    aoligei = pygame.image.load("real_aoligei.jpeg")
-
-    if pressed[pygame.K_w]: fake_y -= 3
-    if pressed[pygame.K_s]: fake_y += 3
-    if pressed[pygame.K_a]: fake_x -= 3
-    if pressed[pygame.K_d]: fake_x += 3
-
-    fake_a = pygame.image.load("aoligei.jpg")
-
-
-
-
-
-
-
-
+    
+    lis = [pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT]
+    x, y = move(pressed, lis, 3, x, y)
 
     miku_icon = pygame.image.load("miku_icon_resize.png")
 
 
 
+    star = pygame.image.load("star_resize.png")
+
+    if(abs(x - star_x) < 15 and abs(y - star_y) < 15):
+        textsurface, score = change_score(score, 1, myfont)
+        star_x = randint(20, 1260)
+        star_y = randint(20, 700)
     
-    window.blit(image, (0, 0))
+
+    
+    window.blit(Back_image, (0, 0))
+    window.blit(star, (star_x, star_y))
     window.blit(miku_icon, (x, y))
-    #window.blit(aoligei, (x, y))
-    #window.blit(fake_a, (fake_x, fake_y))
+    window.blit(textsurface, (0, 0))
     
     pygame.display.flip()
 
--- resize_image.py
+++ resize_image.py
@@ -1,15 +1,15 @@
 import cv2
 
-img = cv2.imread('miku_icon.png', cv2.IMREAD_UNCHANGED)
+img = cv2.imread('star.png', cv2.IMREAD_UNCHANGED)
 
 print('Original Dimensions : ', img.shape)
 
-dim = (40, 40)
+dim = (30, 30)
 resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 
 print('Resized Dimensions : ', resized.shape)
 
-cv2.imwrite('./miku_icon_resize.png', resized)
+cv2.imwrite('./star_resize.png', resized)
 
 cv2.imshow("Resized image", resized)
 cv2.waitKey(0)
